d6b42b1d871dfd07a4f147841e7c5922aa135131
matheusrcruz12 <matheus.cruz12@fatec.sp.gov.br>
2018-07-08 12:34:43 -0300

    commit inicial

---
+++ src/api/modelo/Van.java
@@ -0,0 +1,58 @@
+package api.modelo;
+
+public class Van {
+	private String placa;
+	private int lugares;
+	private boolean arCond;
+	private boolean frigoBar;
+	private boolean tv;
+	
+	public Van() {
+		
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public int getLugares() {
+		return lugares;
+	}
+
+	public void setLugares(int lugares) {
+		this.lugares = lugares;
+	}
+
+	public boolean isArCond() {
+		return arCond;
+	}
+
+	public void setArCond(boolean arCond) {
+		this.arCond = arCond;
+	}
+
+	public boolean isFrigoBar() {
+		return frigoBar;
+	}
+
+	public void setFrigoBar(boolean frigoBar) {
+		this.frigoBar = frigoBar;
+	}
+
+	public boolean isTv() {
+		return tv;
+	}
+
+	public void setTv(boolean tv) {
+		this.tv = tv;
+	}
+	
+	
+	
+	
+
+}
